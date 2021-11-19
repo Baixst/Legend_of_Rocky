bf1c93ab557a231b876b6e336380f88f049608c7
Baixst <bawini97@yahoo.de>
2021-11-19 19:20:13 +0100

    refactored battlesystem by adding utils

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class BattleSystem : MonoBehaviour
@@ -12,7 +11,6 @@ public class BattleSystem : MonoBehaviour
 
     public GameObject infoBox;
     private TextMeshProUGUI infoText;
-    public GameObject damagePopUpPrefab;
 
     public GameObject playerPrefab1;
     public GameObject playerPrefab2;
@@ -20,13 +18,6 @@ public class BattleSystem : MonoBehaviour
     public GameObject enemyPrefab1;
     public GameObject enemyPrefab2;
     public GameObject enemyPrefab3;
-
-    BattleUnit playerUnit1;
-    BattleUnit playerUnit2;
-    BattleUnit playerUnit3;
-    BattleUnit enemyUnit1;
-    BattleUnit enemyUnit2;
-    BattleUnit enemyUnit3;
 
     public BattleHUD playerHUD1;
     public BattleHUD playerHUD2;
@@ -36,23 +27,20 @@ public class BattleSystem : MonoBehaviour
     public BattleHUD enemyHUD3;
 
     public GameObject buttonsParent;
-    private List<BattleButton> battleButtons = new List<BattleButton>();
+    [HideInInspector]
+    public List<BattleButton> battleButtons = new List<BattleButton>();
 
     private List<BattleUnit> turnOrder = new List<BattleUnit>();
     private int turnOrderIndex = 0;
 
-    private int totalPlayerHP = 0;
-    private int totalEnemyHP = 0;
-
-    // public Transform playerPosition;
-    // public Transform enemyPosition;
+    public BattleUtils utils; // TO-DO: change to private
 
     public BattleState state;
 
     void Start()
     {
-        infoText = infoBox.GetComponentInChildren<TextMeshProUGUI>();   
-        infoBox.SetActive(false);     
+        infoText = infoBox.GetComponentInChildren<TextMeshProUGUI>();
+        infoBox.SetActive(false);
         targetSelector.SetActive(false);
         buttonsParent.SetActive(false);
 
@@ -64,93 +52,9 @@ public class BattleSystem : MonoBehaviour
         }
 
         state = BattleState.START;
-        SetupBattle();
-    }
-
-    private void SetupBattle()
-    {
-        // instantiate all battle units and setup their UI components
-        GameObject playerGO1 = Instantiate(playerPrefab1);
-        playerUnit1 = playerGO1.GetComponent<BattleUnit>();
-        playerHUD1.SetHUD(playerUnit1);
-        totalPlayerHP += playerUnit1.currentHP;
-
-        if (playerPrefab2 != null)
-        {
-            GameObject playerGO2 = Instantiate(playerPrefab2);
-            playerUnit2 = playerGO2.GetComponent<BattleUnit>();
-            playerHUD2.SetHUD(playerUnit2);
-            totalPlayerHP += playerUnit2.currentHP;
-        }
-        if (playerPrefab3 != null)
-        {
-            GameObject playerGO3 = Instantiate(playerPrefab3);
-            playerUnit3 = playerGO3.GetComponent<BattleUnit>();
-            playerHUD3.SetHUD(playerUnit3);
-            totalPlayerHP += playerUnit3.currentHP;
-        }
-
-        GameObject enemyGO1 = Instantiate(enemyPrefab1);
-        enemyUnit1 = enemyGO1.GetComponent<BattleUnit>();
-        enemyHUD1.SetHUD(enemyUnit1);
-        totalEnemyHP += enemyUnit1.currentHP;
-
-        if (enemyPrefab2 != null)
-        {
-            GameObject enemyGO2 = Instantiate(enemyPrefab2);
-            enemyUnit2 = enemyGO2.GetComponent<BattleUnit>();
-            enemyHUD2.SetHUD(enemyUnit2);
-            totalEnemyHP += enemyUnit2.currentHP;
-        }
-        if (enemyPrefab3 != null)
-        {
-            GameObject enemyGO3 = Instantiate(enemyPrefab3);
-            enemyUnit3 = enemyGO3.GetComponent<BattleUnit>();
-            enemyHUD3.SetHUD(enemyUnit3);
-            totalEnemyHP += enemyUnit3.currentHP;
-        }
-
-        // setup turn order and start battle
-        SetupTurnOrder();
+        utils.SetupBattle();
+        turnOrder = utils.SetupTurnOrder();
         StartCoroutine(StartBattle());
-    }
-
-    private void SetupTurnOrder()
-    {
-        turnOrder.Add(playerUnit1);
-        turnOrder.Add(enemyUnit1);
-
-        if (playerUnit2 != null)
-        {
-            turnOrder.Add(playerUnit2);
-        }
-        if (playerUnit3 != null)
-        {
-            turnOrder.Add(playerUnit3);
-        }
-        if (enemyUnit2 != null)
-        {
-            turnOrder.Add(enemyUnit2);
-        }
-        if (enemyUnit3 != null)
-        {
-            turnOrder.Add(enemyUnit3);
-        }
-
-        turnOrder = orderByInitiative(turnOrder);
-
-        Debug.Log("First in order: " + turnOrder[0].unitName);
-        Debug.Log("Speed of first unit: " + turnOrder[0].init);
-    }
-
-    private List<BattleUnit> orderByInitiative(List<BattleUnit> list)
-    {
-        // orders list ascending by init stat -> unit with lowest init is first in list
-        list.Sort((x, y) => x.init.CompareTo(y.init));
-
-        // reverse list so that unit with highest init is first in list
-        list.Reverse();
-        return list;
     }
 
     private IEnumerator StartBattle()
@@ -184,7 +88,7 @@ public class BattleSystem : MonoBehaviour
         }
         else
         {
-            updateButtons();
+            utils.updateButtons();
             buttonsParent.SetActive(true);
         }
     }
@@ -213,48 +117,22 @@ public class BattleSystem : MonoBehaviour
         yield return new WaitForSeconds(2); // this wait would be replaced by an attack animation
 
         infoBox.SetActive(false);
-        UpdateAfterMove();
+        utils.UpdateAfterMove();
         yield return new WaitForSeconds(2);
         nextTurn();
-    }
-
-    private void UpdateAfterMove()
-    {
-        // Update HP value in UI 
-        updateHUDs();
-        updateHPTrackers();
-
-        foreach (BattleUnit unit in turnOrder)
-        {
-            if (unit.currentHP < unit.lastTurnHP) // check if unit has taken damage
-            {
-                GameObject DamageText = Instantiate(damagePopUpPrefab);
-                DamageText.transform.position = unit.transform.position;
-                DamageText.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(((unit.lastTurnHP - unit.currentHP).ToString()));
-                unit.lastTurnHP = unit.currentHP;
-            }
-            else if (unit.currentHP > unit.lastTurnHP) // check if unit was healed
-            {
-                GameObject DamageText = Instantiate(damagePopUpPrefab);
-                DamageText.transform.position = unit.transform.position;
-                DamageText.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(((unit.currentHP - unit.lastTurnHP).ToString()));
-                DamageText.transform.GetChild(0).GetComponent<TextMeshPro>().color = new Color32(32, 193, 51, 255); // set text color to green
-                unit.lastTurnHP = unit.currentHP;
-            }
-        } 
     }
 
     private void nextTurn()
     {
         // only play next turn when battle is not won or lost
         if (state == BattleState.WON || state == BattleState.LOST) return;
-        if (totalEnemyHP == 0)
+        if (utils.PlayerWon())
         {
             state = BattleState.WON;
             EndBattle();
             return;
         }
-        else if (totalPlayerHP == 0)
+        else if (utils.EnemyWon())
         {
             state = BattleState.LOST;
             EndBattle();
@@ -265,7 +143,7 @@ public class BattleSystem : MonoBehaviour
         if (turnOrderIndex + 1 == turnOrder.Count)
         {
             // sort turn order again, a units init could have changed
-            turnOrder = orderByInitiative(turnOrder);
+            turnOrder = utils.orderByInitiative(turnOrder);
             turnOrderIndex = 0;
         }
         else
@@ -304,21 +182,21 @@ public class BattleSystem : MonoBehaviour
         infoBox.SetActive(true);
         yield return new WaitForSeconds(1.5f);
 
-        if (playerUnit1.currentHP > 0)
+        if (utils.getPlayerUnits()[0].currentHP > 0)
         {
-            playerUnit1.TakeDamage(turnOrder[turnOrderIndex].phyAtk);
+            utils.getPlayerUnits()[0].TakeDamage(turnOrder[turnOrderIndex].phyAtk);
         }
-        else if (playerUnit2.currentHP > 0)
+        else if (utils.getPlayerUnits()[1].currentHP > 0)
         {
-            playerUnit2.TakeDamage(turnOrder[turnOrderIndex].phyAtk);
+            utils.getPlayerUnits()[1].TakeDamage(turnOrder[turnOrderIndex].phyAtk);
         }
         else
         {
-            playerUnit3.TakeDamage(turnOrder[turnOrderIndex].phyAtk);
+            utils.getPlayerUnits()[2].TakeDamage(turnOrder[turnOrderIndex].phyAtk);
         }
 
         infoBox.SetActive(false);
-        UpdateAfterMove();
+        utils.UpdateAfterMove();
         yield return new WaitForSeconds(1.5f);
 
         nextTurn();
@@ -337,92 +215,18 @@ public class BattleSystem : MonoBehaviour
         infoBox.SetActive(true);
     }
 
-    private void updateButtons()
-    {
-        foreach (BattleButton b in battleButtons)
-        {
-            b.changeTextToActiveUnit();
-        }
-    }
-
-    private void updateHUDs()
-    {
-        playerHUD1.SetHP(playerUnit1.currentHP, playerUnit1);
-        if (playerPrefab2 != null)
-        {
-            playerHUD2.SetHP(playerUnit2.currentHP, playerUnit2);
-        }
-        if (playerPrefab3 != null)
-        {
-            playerHUD3.SetHP(playerUnit3.currentHP, playerUnit3);
-        }
-
-        enemyHUD1.SetHP(enemyUnit1.currentHP, enemyUnit1);
-        if (enemyPrefab2 != null)
-        {
-            enemyHUD2.SetHP(enemyUnit2.currentHP, enemyUnit2);
-        }
-        if (enemyPrefab3 != null)
-        {
-            enemyHUD3.SetHP(enemyUnit3.currentHP, enemyUnit3);
-        }
-    }
-
-    private void updateHPTrackers()
-    {
-        totalPlayerHP = playerUnit1.currentHP;
-        if (playerPrefab2 != null)
-        {
-            totalPlayerHP += playerUnit2.currentHP;
-        }
-        if (playerPrefab3 != null)
-        {
-            totalPlayerHP += playerUnit3.currentHP;
-        }
-
-        totalEnemyHP = enemyUnit1.currentHP;
-        if (enemyPrefab2 != null)
-        {
-            totalEnemyHP += enemyUnit2.currentHP;
-        }
-        if (enemyPrefab3 != null)
-        {
-            totalEnemyHP += enemyUnit3.currentHP;
-        }
-    }
-
     public BattleUnit getActiveUnit()
     {
         return turnOrder[turnOrderIndex];
     }
 
-    public List<BattleUnit> getPartyUnits()
+    public List<BattleUnit> getPlayerUnits()
     {
-        List<BattleUnit> partyUnits = new List<BattleUnit>();
-        partyUnits.Add(playerUnit1);
-        if (playerUnit2 != null)
-        {
-            partyUnits.Add(playerUnit2);
-        }
-        if (playerUnit3 != null)
-        {
-            partyUnits.Add(playerUnit3);
-        }
-        return partyUnits;
+        return utils.getPlayerUnits();
     }
 
     public List<BattleUnit> getEnemyUnits()
     {
-        List<BattleUnit> enemyUnits = new List<BattleUnit>();
-        enemyUnits.Add(enemyUnit1);
-        if (enemyUnit2 != null)
-        {
-            enemyUnits.Add(enemyUnit2);
-        }
-        if (enemyUnit3 != null)
-        {
-            enemyUnits.Add(enemyUnit3);
-        }
-        return enemyUnits;
+        return utils.getEnemyUnits();
     }
 }
