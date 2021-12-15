d2a4206112b573b4fdaa710ade6249e987d2a25c
Baixst <bawini97@yahoo.de>
2021-12-15 18:12:17 +0100

    added dialog system from felix

---
+++ Assets/Animations/Scripts/Dialog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialog
+{
+    public string name;
+    public string[] lines;
+    public int nextDialogIndex;
+}
