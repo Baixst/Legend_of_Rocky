be6a649fdeb9ae4140c56158ff5d1d84bd7492b2
Baixst <bawini97@yahoo.de>
2022-02-07 11:33:50 +0100

    audio transitions, looped "nonsense"
     added move prefabs to units again

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,6 +7,15 @@ public class SceneLoader : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
+    public AudioSource music;
+
+    void Start()
+    {
+        if(music != null)
+        {
+            StartCoroutine(FadeInAudio(music, transitionTime));
+        }
+    }
 
     public void LoadScene(int sceneIndex)
     {
@@ -30,15 +39,48 @@ public class SceneLoader : MonoBehaviour
 
     IEnumerator LoadSceneByIndex(int sceneIndex)
     {
+        if (music != null)
+        {
+            StartCoroutine(FadeOutAudio(music, transitionTime));
+        }
         transition.SetTrigger("StartTransition");
-        yield return new WaitForSeconds(transitionTime);
+        yield return new WaitForSeconds(transitionTime + 0.1f);
         SceneManager.LoadScene(sceneIndex);
     }
 
     IEnumerator LoadSceneByString(string sceneName)
     {
+        if (music != null)
+        {
+            StartCoroutine(FadeOutAudio(music, transitionTime));
+        }
         transition.SetTrigger("StartTransition");
-        yield return new WaitForSeconds(transitionTime);
+        yield return new WaitForSeconds(transitionTime + 0.1f);
         SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator FadeOutAudio(AudioSource audioSource, float fadeTime)
+    {
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+            yield return null;
+        }
+ 
+        audioSource.Stop ();
+        audioSource.volume = startVolume;
+    }
+
+    private IEnumerator FadeInAudio(AudioSource audioSource, float fadeTime)
+    {
+        float startVolume = audioSource.volume;
+        audioSource.volume = 0;
+        while (audioSource.volume < startVolume)
+        {
+            audioSource.volume += startVolume * Time.deltaTime / fadeTime;
+            yield return null;
+        }
+        audioSource.volume = startVolume;
     }
 }
